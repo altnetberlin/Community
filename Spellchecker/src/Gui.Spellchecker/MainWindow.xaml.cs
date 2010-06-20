016c459ca5ac8e9443d78af13b42bf60cbebaf97
Robert Mischke <robert@speak-friend.com>
2010-06-20 09:56:19 +0200

    Initiale Struktur, Namespaces GUI

--- Spellchecker/src/Gui.Spellchecker/MainWindow.xaml.cs
+++ Spellchecker/src/Gui.Spellchecker/MainWindow.xaml.cs
@@ -12,7 +12,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace Gui.Spellchecker
+namespace Rm.Spellchecker.Gui
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
