a66cb3d31a067e87acb689d237ed53ebb0327542
Robert Mischke <robert@speak-friend.com>
2010-07-01 21:32:00 +0200

    Release 1 (Kundenpräsentation)

---
+++ CodingDojo4/scr/Core/Core/ArabicToRomanConverter.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Core
+{
+    public class ArabicToRomanConverter
+    {
+        public string Convert(string numberToConvert)
+        {
+            switch (numberToConvert)
+            {
+                case "1900":
+                    return "MCM";
+                case "1901":
+                    return "MCMI";
+                case "1902":
+                    return "MCMII";
+                case "1903":
+                    return "MCMIII";
+                case "1904":
+                    return "MCMIV";
+                case "1905":
+                    return "MCMV";
+                case "1906":
+                    return "MCMVI";
+                case "1907":
+                    return "MCMVII";
+                case "1908":
+                    return "MCMVIII";
+                case "1909":
+                    return "MCMIX";
+
+            }
+
+            return "MCMYXS"; //very complex the customer will be delighted
+        }
+    }
+}
---
+++ CodingDojo4/scr/Frontend/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Core;
+
+namespace Frontend
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var input = Console.ReadLine();
+
+            var converter = new ArabicToRomanConverter();
+            Console.WriteLine(converter.Convert(input));
+            Console.ReadLine();
+        }
+    }
+}
