5118f2d7a46642b3bdb3ce9ac226de410d78dd97
Robert Mischke <robertmischke@gmail.com>
2010-09-30 16:01:14 +0200

    BDDFrameworks: Domainmodel für BDD Beispiele

---
+++ BDDFrameworksVergleich/scr/Core/Model/Auftrag/Auftrag.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Core.Model
+{
+	public class Auftrag
+	{
+		public int Id;
+		
+		public Käufer Käufer;
+		public Verkäufer Verkäufer;
+
+		public Positionen Positionen ;
+
+		public decimal Gesamtpreis { get { return Positionen.GetGesamtPreis(); } }
+
+	}
+}
---
+++ BDDFrameworksVergleich/scr/Core/Model/Rollen/Verkaeufer.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Core.Model
+{
+	public class Verkäufer
+	{
+		public int Id;
+		public string Name;
+	}
+}
