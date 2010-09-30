ab2a69326c9a4ff10926fb20ed79ba111c1de0f7
Robert Mischke <robertmischke@gmail.com>
2010-09-30 17:15:00 +0200

    BDDFrameworks: FantasyFormatExporter

---
+++ BDDFrameworksVergleich/scr/Core/Schnittstellen/FantasyFormat/FantasyFormatExporter.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Xml.Linq;
+
+namespace Core.Schnittstellen
+{
+	public class FantasyFormatExporter
+	{
+
+		public void Run(FantasyFormatExporterCommand cmd)
+		{
+			var auftrag = cmd.Auftrag;
+
+			var xDoc = new XDocument(
+				new XElement("Auftrag"),
+					new XElement("Id", auftrag.Id),
+					new XElement("Käufer",
+						new XElement("Id", auftrag.Käufer.Id),
+						new XElement("Name", auftrag.Käufer.Name)),
+					new XElement("Verkäufer",
+						new XElement("Id", auftrag.Verkäufer.Id),
+						new XElement("Name", auftrag.Verkäufer.Name)),
+					new XElement("Positionen"),
+						auftrag.Positionen.Select(
+							pos => new XElement("Position",
+								new XElement("Id", pos.Id),
+								new XElement("Preis", pos.Preis))));
+
+			xDoc.Save(cmd.OutputPath);
+		}
+
+	}
+}
---
+++ BDDFrameworksVergleich/scr/Core/Schnittstellen/FantasyFormat/FantasyFormatExporterCommand.cs
@@ -0,0 +1,10 @@
+﻿using Core.Model;
+
+namespace Core.Schnittstellen
+{
+	public class FantasyFormatExporterCommand
+	{
+		public Auftrag Auftrag;
+		public string OutputPath = "out.xml";
+	}
+}
